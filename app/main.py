5b9c6da9bce16960116a228db4b683d82ba058d0
wallseat <wallseat@gmail.com>
2022-04-03 16:38:16 +0300

    Changed nginx conf, move some variables to env and other

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from .api.router import api_router
 from .db import engine
 from .models import metadata
 
-app = FastAPI(openapi_url="/api/openapi.json")
+app = FastAPI(openapi_url="/api/openapi.json", docs_url="/api/docs")
 
 
 @app.on_event("startup")
